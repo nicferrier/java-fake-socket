09ebb8a927c18eca54bc29dd19eed589e76d4645
Nic Ferrier <nferrier@ferrier.me.uk>
2015-03-17 06:08:46 +0000

    move everything again, to a src dir.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 DIR=$(cd $(dirname $0) ; pwd)
 mkdir -p $DIR/classes
 rm -rf classes/*
-javac -d $DIR/classes $DIR/{Address,FakeSocketImpl,CommunicatingSocket,Backplane,TestFakeSocket}.java
+javac -d $DIR/classes $DIR/src/{Address,FakeSocketImpl,CommunicatingSocket,Backplane,TestFakeSocket}.java
 java -classpath $DIR/classes fakesocket.TestFakeSocket
